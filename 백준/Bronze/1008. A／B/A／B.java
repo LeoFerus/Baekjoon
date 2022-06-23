9c6e7a63e993fa3aeac0dd404b0a6a158ae3a061
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-06-23 23:29:36 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.*; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextInt(); 
+		double b = scan.nextInt();
+		
+		System.out.print(a/b);
+	
+		scan.close();
+	}
+
+}
