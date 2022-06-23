a88ee8fbb691126053ef6cf5de61df86e83d9f11
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-06-23 23:56:02 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.next(); 
+		
+		System.out.println(a+"??!");
+
+		scan.close();
+	}
+
+}
