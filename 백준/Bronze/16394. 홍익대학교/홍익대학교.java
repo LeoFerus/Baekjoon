0bd51c3667200082d47308e6c44fe516dccc0f8a
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-06-29 00:02:55 +0900

    [Bronze V] Title: 홍익대학교, Time: 224 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextInt();
+		
+		System.out.println(n-1946);
+		
+		sc.close();
+
+	}
+
+}
