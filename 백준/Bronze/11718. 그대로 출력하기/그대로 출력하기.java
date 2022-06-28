bd628b26f4fdf90126eb3a43689994238c28e621
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-06-28 19:10:29 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 244 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNext()) {
+			
+		System.out.println(scan.nextLine());
+
+		
+		}
+
+	}
+
+}
