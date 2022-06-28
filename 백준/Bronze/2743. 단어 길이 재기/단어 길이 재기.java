e1c11f53de025c89a996bdfa6df3f18841dbc722
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-06-28 19:59:30 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		String s = scan.next();
+		
+		String[]array = s.split("");
+		
+		System.out.println(array.length);
+		
+		scan.close();
+
+	}
+
+}
