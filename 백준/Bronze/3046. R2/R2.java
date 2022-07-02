681628d24086e20e169e19a759c61a3ee42e7721
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-07-02 10:40:22 +0900

    [Bronze IV] Title: R2, Time: 212 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		long r1 = sc.nextLong();
+		
+		long s = sc.nextLong();
+		
+		
+		long r2;
+		
+		r2 = 2*s-r1;
+		
+		System.out.println(r2);
+
+		
+		sc.close();
+	}
+
+}
---
+++ 백준/Bronze/3046. R2/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] R2 - 3046 
+
+[문제 링크](https://www.acmicpc.net/problem/3046) 
+
+### 성능 요약
+
+메모리: 17656 KB, 시간: 212 ms
+
+### 분류
+
+구현(implementation), 사칙연산(arithmetic), 수학(math)
+
