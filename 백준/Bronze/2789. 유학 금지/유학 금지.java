20653e9530a144ec59fe1eca663ad55bb94723db
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-07-13 10:20:12 +0900

    [Bronze II] Title: 유학 금지, Time: 204 ms, Memory: 17552 KB -BaekjoonHub

--- 백준/Bronze/2789. 유학 금지/유학 금지.java
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String w = sc.next();
-		
-		for(int i = 0; i<w.length(); i++) {
-			if(w.charAt(i)!='C'&&w.charAt(i)!='A'&&w.charAt(i)!='M'&&w.charAt(i)!='B'&&w.charAt(i)!='R'&&w.charAt(i)!='I'&&w.charAt(i)!='D'&&w.charAt(i)!='G'&&w.charAt(i)!='E') {
-				System.out.print(w.charAt(i));
-			}
-		}
-		sc.close();
-	}
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+      String [] w = sc.next().split("");
+      
+      for(int i = 0; i<w.length; i++) {
+         if(!w[i].equals("C")&&!w[i].equals("A")&&!w[i].equals("M")&&!w[i].equals("B")&&!w[i].equals("R")&&!w[i].equals("I")&&!w[i].equals("D")&!w[i].equals("G")&&!w[i].equals("E")) {
+            System.out.print(w[i]);
+         }
+      }
+      sc.close();
+   }
  
 }
