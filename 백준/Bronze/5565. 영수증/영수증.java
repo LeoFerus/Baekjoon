bea51d2c5231805d7af91b332e4ab81ef3d34b14
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-07-23 21:01:41 +0900

    [Bronze III] Title: 영수증, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+				
+		for(int i = 0; i<9;i++) {
+			int books = sc.nextInt();
+			t = t-books;
+		}
+		
+		System.out.println(t);
+		sc.close();
+	}
+}
