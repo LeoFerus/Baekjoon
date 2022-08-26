ab1da02a10ac1481f78150e3dcc949a459dc9f29
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-08-26 20:46:53 +0900

    [Bronze III] Title: 전체 계산 횟수, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/17174. 전체 계산 횟수/전체 계산 횟수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        int m = sc.nextInt();
+        
+        int cnt = n;
+        
+        int check = n;
+        
+        while(check>=m) {
+        	cnt += check/m;
+        	check/=m;
+        }
+        
+     
+        	System.out.println(cnt);
+        
+        sc.close();
+    }
+}
