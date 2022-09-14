54152485d8a14d5e2faf6d03b67cc16bf2c858c2
Mad_Tea-drinker <cmdytkah12@naver.com>
2022-09-14 20:25:13 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 252 ms, Memory: 17892 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,18 @@
+	import java.util.*;
+	
+	public class Main {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	
+	        String str = sc.nextLine();
+	     
+	        
+	        if(str.contains("D2")||str.contains("d2")) {
+	        	System.out.println("D2");
+	        }
+	        else {
+	        	System.out.println("unrated");
+	        }
+	    }
+	    	
+	}
