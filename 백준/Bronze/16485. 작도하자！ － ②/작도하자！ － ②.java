6a11c442691306c440a48f7b7d5fe8fe556384bf
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-10-19 19:21:50 +0900

    [Bronze III] Title: 작도하자! - ②, Time: 216 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/16485. 작도하자！ － ②/작도하자！ － ②.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+	public static void main(String[] args)throws IOException   {
+
+		Scanner sc = new Scanner(System.in);
+		
+		double c = sc.nextDouble();
+		
+		double b = sc.nextDouble();
+		
+		double r = c/b;
+		
+		System.out.println(r);
+		
+		
+		
+		sc.close();
+		
+
+		
+	}
+
+}	
