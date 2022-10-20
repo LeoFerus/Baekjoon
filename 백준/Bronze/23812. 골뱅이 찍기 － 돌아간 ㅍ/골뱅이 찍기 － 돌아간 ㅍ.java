8a9c2300c3a6378e294ed4a3380cd4843041d410
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-10-20 19:41:34 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 300 ms, Memory: 19496 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+	public static void main(String[] args)throws IOException   {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		for(int i = 0; i<n; i++) {
+		System.out.println("@".repeat(n)+"   ".repeat(n)+"@".repeat(n));
+		}
+		
+		for(int i = 0; i<n; i++) {
+			System.out.println("@@@@@".repeat(n));
+		}
+		for(int i = 0; i<n; i++) {
+			System.out.println("@".repeat(n)+"   ".repeat(n)+"@".repeat(n));
+			}
+		for(int i = 0; i<n; i++) {
+			System.out.println("@@@@@".repeat(n));
+		}
+		for(int i = 0; i<n; i++) {
+			System.out.println("@".repeat(n)+"   ".repeat(n)+"@".repeat(n));
+			}
+		sc.close();
+		
+
+		
+	}
+
+}	
