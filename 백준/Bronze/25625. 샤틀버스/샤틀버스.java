c47d528a82014cbaceb9f1c2d421b228e01be4dd
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-11-05 20:58:01 +0900

    [Bronze IV] Title: 샤틀버스, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*; 
+
+public class Main {
+	public static void main(String[] args)throws IOException   {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		
+		int y = sc.nextInt();
+		
+		if(x>y) {
+			System.out.println(x+y);
+		}
+		else {
+			System.out.println(y-x);
+		}
+
+		sc.close();
+	}	
+}
