334ac7136fcafa7dc2162f6182df3badfb8c46bf
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-11-29 16:48:52 +0900

    [Bronze V] Title: WARBOY, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    int a = sc.nextInt();
+    
+    int b = sc.nextInt();
+    
+    int c = sc.nextInt();
+    
+    System.out.println(b/a*3*c);
+    
+	}
+}
