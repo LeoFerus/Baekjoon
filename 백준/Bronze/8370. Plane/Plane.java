f50b6ba5ef0e921ff730546c3406e6346a9e153f
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-03 20:46:05 +0900

    [Bronze V] Title: Plane, Time: 212 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    int n1  =sc.nextInt();
+    
+    int k1 = sc.nextInt();
+    
+    int n2 = sc.nextInt();
+    
+    int k2 = sc.nextInt();
+    
+    
+    System.out.println((n1*k1)+(n2*k2));
+	}
+}
