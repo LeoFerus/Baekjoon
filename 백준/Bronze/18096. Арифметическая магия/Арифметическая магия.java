cf6374a8b9880c0b80bfc4181ca7b1d187f6cc1e
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-07 22:46:56 +0900

    [Bronze V] Title: Арифметическая магия, Time: 208 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println(1);
+
+    
+	}
+}
