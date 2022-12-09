1884b8b9dcc1f1d05aafc01d2a7225673411b3a4
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-09 23:55:30 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 224 ms, Memory: 18228 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    String n = sc.next();
+    
+    System.out.println(":fan::fan::fan:");
+    System.out.println(":fan::"+n+"::fan:");
+    System.out.println(":fan::fan::fan:");
+	}
+}
