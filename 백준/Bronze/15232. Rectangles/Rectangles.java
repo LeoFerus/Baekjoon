34e6dfd66a5c40dc1400a972da27b7e0aa86ce42
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-14 21:37:55 +0900

    [Bronze IV] Title: Rectangles, Time: 208 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    
+    int r = sc.nextInt();
+    
+    int c = sc.nextInt();
+    
+    for(int i = 0; i<r; i++) {
+    	
+    	System.out.println("*".repeat(c));
+    }
+    
+    
+    
+    
+		  
+	  
+	}
+}
