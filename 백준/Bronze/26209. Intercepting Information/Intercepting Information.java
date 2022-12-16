6f39563ba50725d4221a81b35751be79e2c01e28
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-17 00:50:56 +0900

    [Bronze V] Title: Intercepting Information, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    
+     String n =sc.nextLine();
+     
+     if(n.contains("9")) {
+    	 System.out.println("F");
+     }
+     else {
+    	 System.out.println("S");
+     }
+    
+    
+    
+		  
+	  
+	}
+}
