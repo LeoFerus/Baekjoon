5167f9df50940dcf6d35a75c9a6d1b43b2cbe122
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-17 06:30:40 +0900

    [Bronze IV] Title: Squares, Time: 228 ms, Memory: 18212 KB -BaekjoonHub

---
+++ 백준/Bronze/6887. Squares/Squares.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    
+     int n = sc.nextInt();
+     
+     double result = 0;
+     
+    
+    	 
+    	 result = Math.sqrt(n);
+    	
+    	 System.out.println("The largest square has side length "+Math.round(Math.floor(result))+".");
+    
+	  
+	}
+}
