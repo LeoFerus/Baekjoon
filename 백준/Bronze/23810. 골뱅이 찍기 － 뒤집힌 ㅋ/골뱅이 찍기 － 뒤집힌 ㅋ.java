ed5201bae99d2eaa34fef0ae7b271a777f496a50
Mad_Tea-drinker <skhu201713005@gmail.com>
2022-12-21 09:18:36 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 256 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    
+    for(int i =0; i<n;i++) {
+    	System.out.println("@@@@@".repeat(n));
+    }
+    for(int i =0;i<n;i++) {
+    	System.out.println("@".repeat(n));
+    }
+    for(int i =0; i<n;i++) {
+    	System.out.println("@@@@@".repeat(n));
+    }
+    for(int i =0;i<n;i++) {
+    	System.out.println("@".repeat(n));
+    }
+    for(int i =0;i<n;i++) {
+    	System.out.println("@".repeat(n));
+    }
+    
+	}
+}
