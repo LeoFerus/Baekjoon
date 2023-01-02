fe978aaceb5a5046a52319b1f9544ab322a28f38
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-01-03 00:35:56 +0900

    [Bronze III] Title: 접시 안의 원, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	     double t = sc.nextInt();
+	     
+	     System.out.println(Math.round(Math.pow(t/2,2)));
+
+           
+	}
+}
