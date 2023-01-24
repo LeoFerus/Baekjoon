d47c19043f142543c4b9d569fb35f05184c5792b
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-01-24 23:30:54 +0900

    [Bronze V] Title: Copier, Time: 232 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		int x = 0;
+		
+		for(int i = 0; i<t; i++) {
+			
+			x = sc.nextInt();
+			System.out.println(x+" "+x);
+		}
+		
+	
+		
+		sc.close();
+		
+		
+	}
+}
