3ae80d883518e49fc50d27fbbccc60640c7873df
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-03-04 23:21:23 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+		
+	int wc =sc.nextInt();
+	
+	int hc = sc.nextInt();
+	
+	int ws =sc.nextInt();
+	
+	int hs = sc.nextInt();
+	
+	if(wc-ws >=2&&hc-hs>=2) {
+		System.out.println(1);
+	}
+	else {
+		System.out.println(0);
+	}
+		
+		
+		
+	}
+}
