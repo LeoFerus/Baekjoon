890fa1e0d1d0d09df8e96afd72b34a397886aed5
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-04-05 23:50:19 +0900

    [Bronze III] Title: Time to Decompress, Time: 208 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for(int i = 0 ; i<t; i++) {
+			int n = sc.nextInt();
+			
+			String s = sc.next();
+			
+			System.out.println(s.repeat(n));
+		}
+	}
+}
