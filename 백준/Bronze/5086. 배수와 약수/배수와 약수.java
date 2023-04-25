e9e1f2cbe7add671f7708193a2d35726c2b99055
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-04-25 15:08:20 +0900

    [Bronze III] Title: 배수와 약수, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,29 +1,36 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
- 
+import java.util.stream.IntStream;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
+
+	public static void main(String[] args){
+	
+		Scanner sc = new Scanner(System.in);
+		
 		while(true) {
+			int x= sc.nextInt();
 			
-			int first = in.nextInt();
-			int second = in.nextInt();
+			int y = sc.nextInt();
 			
-			if(first == 0 && second == 0) break;
+			if(x==0&&y==0) {
+				break;
+			}
 			
-			if(second % first == 0) {
+			if(y%x==0) {
 				System.out.println("factor");
 			}
-			else if(first % second == 0) {
+			else if(x%y==0) {
 				System.out.println("multiple");
+				
 			}
 			else {
 				System.out.println("neither");
 			}
 		}
- 
+		
 	}
- 
+
 }
