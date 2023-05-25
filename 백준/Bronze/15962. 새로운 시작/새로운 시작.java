d0919d7a9217e2f18d6efe1ad3aea9e029ea42ec
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-05-25 23:56:05 +0900

    [Bronze V] Title: 새로운 시작, Time: 196 ms, Memory: 17428 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	    Scanner sc = new Scanner(System.in);   
+		
+        
+	 
+        System.out.println("파이팅!!");
+	    
+	    
+	}
+	
+}
