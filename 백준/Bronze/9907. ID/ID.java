0e30b5411e5463900b3c8c0e1f1e07342f24f0d9
Mad_Tea-drinker <skhu201713005@gmail.com>
2023-05-31 00:01:50 +0900

    [Bronze III] Title: ID, Time: 196 ms, Memory: 17460 KB -BaekjoonHub

---
+++ 백준/Bronze/9907. ID/ID.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main { 
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        int sum = 0;
+        int j = 2;
+
+        for (int i = 0; i < N.length(); i++) {
+            if (j < 2) {
+                j = 7;
+            }
+            sum += (N.charAt(i) - '0') * j;
+            j--;
+        }
+
+        String str = "JABCDEFGHIZ";
+        char ch = str.charAt(sum % 11);
+
+        System.out.println(ch);
+        sc.close();
+    }
+}
