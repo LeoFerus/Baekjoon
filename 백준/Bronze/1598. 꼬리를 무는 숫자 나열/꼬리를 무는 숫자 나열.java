0116bb89a067e4f9e93cb2836c53f90af796d087
Mad_Tea-drinker <skhu201713005@gmail.com>
2025-03-17 09:47:27 +0900

    [Bronze III] Title: 꼬리를 무는 숫자 나열, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	    Scanner sc = new Scanner(System.in);   
+		
+	    
+	 
+	 	int a = sc.nextInt()-1;
+	 	int b = sc.nextInt()-1;
+	 	
+	 	System.out.println(Math.abs(a/4-b/4)+ Math.abs(a%4-b%4));
+	 	
+	    
+	 	
+	    
+	}
+	
+}
